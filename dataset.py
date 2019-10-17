a28ceabec85f8d121eb22f43db8339a40b391ab7
Andrew <andrewli128@gmail.com>
2019-10-17 15:38:17 -0700

    Reddit headlines

---
+++ dataset.py
@@ -0,0 +1,10 @@
+import praw
+
+def generate():
+    return get_worldnews_hot_25()
+
+def get_worldnews_hot_25():
+    r = praw.Reddit('bot1', user_agent='worldnews_get_hot_25')
+    submissions = r.subreddit('worldnews').hot(limit=25)
+    return [str(x.title) for x in submissions]
+
