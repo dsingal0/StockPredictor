5302ecddf20f81c13e49521af0adf08767706520
dhruv singal <dhruvsingalabc@gmail.com>
2019-10-24 14:42:24 -0700

    removed learn files. run dataset.py to get the top headlines once

--- dataset.py
+++ dataset.py
@@ -7,4 +7,5 @@ def get_worldnews_hot_25():
     r = praw.Reddit('bot1', user_agent='worldnews_get_hot_25')
     submissions = r.subreddit('worldnews').hot(limit=25)
     return [str(x.title) for x in submissions]
-
+if __name__=="__main__":
+    print(generate())
